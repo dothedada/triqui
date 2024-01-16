aaaffe4b83d0805c9032d2a460156828c8d5f97d
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-01-16 16:30:08 -0500

    Finaliza la implementacion del juego desde la consola

--- main.js
+++ main.js
@@ -1,77 +1,62 @@
-// Tablero
 const board = (() => {
     const tiles = [ ['', '', ''], ['', '', ''], ['', '', ''] ]
 
-    const printTile = (row, col = 0) => tiles[row][col] 
+    const printTile = (row, col) => tiles[row][col] 
     const printRow = row => tiles[row]
     const printCol = col => tiles.map(arr => arr[col])
     const printDiag1 = () => tiles.map((arr, index) => arr[index])
     const printDiag2 = () => tiles.toReversed().map((arr, index) => arr[index])
+    const setMark = (mark, row, col) => { tiles[row][col] = mark }
 
-    const setMark = (mark, row, col) => {
-        if(printTile(row, col)) {
-            window.alert('La casilla ya está marcada, selecciona otra')
-            return
-        }
-        tiles[row][col] = mark
-    }
-
-    return { printTile, printRow, printCol, printDiag1, printDiag2, setMark, tiles }
+    return { printTile, printRow, printCol, printDiag1, printDiag2, setMark }
 })()
 
-// jugadores
 const players = (() => {
     const players = []
 
-    const make = (str) => {
+    const make = (nombre) => {
         players.push({ 
-            name: str ? str : `Computadora ${players.length + 1}`, 
+            name: nombre ? nombre : `Computadora ${players.length + 1}`, 
             mark: !players.length ? 'X' : 'O'
         })
     }
-
-    const getMark = (num) => players[num].mark
-    const getName = (num) => players[num].name
+    const getMark = (player) => players[player].mark
+    const getName = (player) => players[player].name
 
     return { make, getMark, getName }
 })()
 
-players.make('miguel')
-players.make('andea')
-
-// Flujo de juego
 const gameFlow = (() => {
-
     function endGame(mark, row, col) {
-        // Fila
-        if (board.printRow(row).every(cell => cell === mark)) {
-            window.alert(`${mark} ganó, fila`)
-            return true
-        }
-        // Columna
-        if (board.printCol(col).every(cell => cell === mark)) {
-            window.alert(`${mark} ganó, columna`)
-            return true
-        }
-        // Diagonal
+        if (board.printRow(row).every(cell => cell === mark)) return true
+        if (board.printCol(col).every(cell => cell === mark)) return true 
         if (board.printDiag1().every(cell => cell === mark) ||
-            board.printDiag2().every(cell => cell === mark)) {
-            window.alert(`${mark} ganó, diagonal`)
-            return true
-        }
+            board.printDiag2().every(cell => cell === mark)) return true
 
         return false
     }
 
-    for(let round = 0; round < 9; round++) {
+    let round = 0
+    while(round < 10) {
+        if (round === 9) return window.alert('Es un empate')
+
         const row = +window.prompt(`${players.getName(round % 2)} selecciona una fila`)
         const col = +window.prompt(`${players.getName(round % 2)} selecciona una columna`)
 
+        if (board.printTile(row, col)) {
+            window.alert('la celda ya está ocupada')
+            continue
+        }
+
         board.setMark(players.getMark(round % 2), row, col)
-        console.log(board.tiles)
 
-        if(endGame(players.getMark(round % 2), row, col)) break
-        
+        if (endGame(players.getMark(round % 2), row, col)) {
+            window.alert(`${players.getName(round % 2)} ganó.`)
+            return
+        }
 
+        round++
     }
 })()
+// Implementación de la interfase
+
