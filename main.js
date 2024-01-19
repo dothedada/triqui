0ddae2888b8cd3fa71e0454ee71b19ab872e99d9
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-01-19 11:10:50 -0500

    Incorpora los estados de juego al JS

--- main.js
+++ main.js
@@ -41,59 +41,79 @@ const players = (() => {
     const players = []
 
     const make = (nombre) => {
+        if (players.length > 1) return
         players.push({ 
             name: nombre ? nombre : `Jugador ${players.length + 1}`, 
-            mark: !players.length ? 'X' : 'O'
+            mark: !players.length ? 'X' : 'O',
+            score: 0
         })
     }
     const getMark = (player) => players[player].mark
     const getName = (player) => players[player].name
+    const getScore = (player) => players[player].score
+    const setScore = (player) => players[player].score++
+    // const shuffleOrder = () => {
+        // if(Math.floor(Math.random() * 2)) {
+            // [players[0], players[1]] = [players[1], players[0]]
+            // console.log('ver')
+        // }
+    // }
 
-    return { make, getMark, getName }
+    return { make, getMark, getName, getScore, setScore }
 })()
 
 players.make('MMejia')
 players.make('Andrea')
 
 const gameFlow = (() => {
-    let state
-    const setState = (num) => {
-        state = num
+    const state = { moment: '', round: '', turn: '' }
+    const set = (moment, round, turn) => {
+        state.moment = moment
+        state.round = round
+        state.turn = turn
         events.pub('state', state)
     }
-    const nextState = () => { 
-        state = (state + 1) % 2 
-        return state
-    }
 
-    function endGame(mark, row, col) {
+    const start = () => set('start', 0, 0)
+    const play = () => set('play', state.round, state.turn + 1)
+    const endRound = () => {
+        players.shuffleOrder()
+        set('endRound', state.round + 1, 0)
+    }
+    const endGame = () => set('endGame', state.round, 0)
+
+    function evalMatch(mark, row, col) {
+        // victoria
         if (board.getRow(row).every(cell => cell === mark)) return true
         if (board.getCol(col).every(cell => cell === mark)) return true 
         if (board.getDiag1().every(cell => cell === mark) ||
             board.getDiag2().every(cell => cell === mark)) return true
-        if (!board.getBoard().includes('')) return true
+        // empate
 
         return false
     }
 
-    return { endGame , setState, nextState }
+    return { start, play, endRound, endGame, evalMatch, state }
 })()
 
 const interfase = (() => {
     const gameBoard = document.querySelectorAll('.board > button')
     const mainMessage = document.getElementById('mainMessage')
     
-    const drawBoard = (state = gameFlow.nextState()) => {
-        if (state === 2) {
+    const drawBoard = ( { moment, round, turn } ) => {
+
+        console.log (moment, round, turn)
+        if (moment === 'start') {
             mainMessage.textContent = '¿Quienes van a jugar?'
             for (const tile of gameBoard) tile.disabled = true
         }
-        if (state === 3) {
+
+        if (moment === 'endRound') {
             mainMessage.textContent = '¡Fin de la ronda!'
             for (const tile of gameBoard) tile.disabled = true
         }
 
-        if (state < 2) {
+        if (moment === 'play') {
             mainMessage.textContent = `¡${players.getName(state)}, tu turno!`
             for (const [index, tile] of gameBoard.entries()) {
                 const row = Math.floor(index / 3)
@@ -115,9 +135,10 @@ const interfase = (() => {
             const row = tile.getAttribute('data-tile')[0]
             const col = tile.getAttribute('data-tile')[2]
             board.setTile(tile.textContent, row, col)
-            events.pub('state', gameFlow.nextState())
+            players.shuffleOrder()
+            // events.pub('state', gameFlow.nextState())
 
-            if (gameFlow.endGame(tile.textContent, row, col)) {
+            if (gameFlow.evalMatch(tile.textContent, row, col)) {
                 window.alert('final')
             }
 
@@ -127,6 +148,5 @@ const interfase = (() => {
     return { drawBoard }
 })()
 
-gameFlow.setState(0)
-
+gameFlow.start()
 
